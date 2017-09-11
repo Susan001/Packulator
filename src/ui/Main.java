d4803c2f291c44d1eda30b8f20604f94be432297
Laura Khaze <laura.khaze@hpe.com>
2017-09-11 15:23:19 +0200

    Add grey pane

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -12,8 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Packulator P2000");
+        primaryStage.setScene(new Scene(root, 400, 550));
         primaryStage.show();
     }
 
